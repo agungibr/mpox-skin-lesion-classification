8b086acfbab35fda9e7d50cbf0543a8ccf706ded
agungibr <agungmalik050@gmail.com>
2024-11-27 23:36:30 +0700

    feat: resize

--- utils/getData.py
+++ utils/getData.py
@@ -9,39 +9,41 @@ class getImageLabel(Dataset):
     def __init__ (self, 
                   augmented = './dataset/Augmented Images/Augmented Images/FOLDS_AUG/',
                   original = './dataset/Original Images/Original Images/FOLDS/',
-                  folds = [1, 2, 3, 4, 5],):
+                  folds = [1, 2, 3, 4, 5]):
         
         self.dataset = []
         to_one_hot = np.eye(6)
 
         for fold in folds:
-            ori_train = join(original, f"fold{fold}/Train/")
+            ori_train = (original + f"fold{fold}/Train")
             for i, pox in enumerate(sorted(listdir(ori_train))):
                 for image_name in listdir(ori_train + "/" + pox):
-                    image = cv.resize(cv.imread(ori_train + "/" + pox + "/" + image_name), (224, 224)) / 255
+                    image = cv.resize(cv.imread(ori_train + "/" + pox + "/" + image_name), (32, 32)) / 255
                     self.dataset.append([image, to_one_hot[i]])
             
-            aug = join(augmented, f"fold{fold}_AUG/Train/")
+            aug = (augmented + f"fold{fold}_AUG/Train")
             for i, pox in enumerate(sorted(listdir(aug))):
                 for image_name in listdir(aug + "/" + pox):
-                    image = cv.resize(cv.imread(aug + "/" + pox + "/" + image_name), (224, 224)) / 255
+                    image = cv.resize(cv.imread(aug + "/" + pox + "/" + image_name), (32, 32)) / 255
                     self.dataset.append([image, to_one_hot[i]])
 
-            ori_test = join(original, f"fold{fold}/Test/")
+            ori_test = (original + f"fold{fold}/Test")
             for i, pox in enumerate(sorted(listdir(ori_test))):
                 for image_name in listdir(ori_test + "/" + pox):
-                    image = cv.resize(cv.imread(ori_test + "/" + pox + "/" + image_name), (224, 224)) / 255
+                    image = cv.resize(cv.imread(ori_test + "/" + pox + "/" + image_name), (32, 32)) / 255
                     self.dataset.append([image, to_one_hot[i]])
 
-            ori_valid = join(original, f"fold{fold}/Valid/")
+            ori_valid = (original + f"fold{fold}/Valid")
             for i, pox in enumerate(sorted(listdir(ori_valid))):
                 for image_name in listdir(ori_valid + "/" + pox):
-                    image = cv.resize(cv.imread(ori_valid + "/" + pox + "/" + image_name), (224, 224)) / 255
+                    image = cv.resize(cv.imread(ori_valid + "/" + pox + "/" + image_name), (32, 32)) / 255
                     self.dataset.append([image, to_one_hot[i]])
             
     def __getitem__(self, item):
         feature, label = self.dataset[item]
-        return torch.tensor(feature, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
+        #return torch.tensor(feature, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
+        label_index = torch.argmax(torch.tensor(label, dtype=torch.float32))  # Convert one-hot to class index
+        return torch.tensor(feature, dtype=torch.float32), label_index
     
     def __len__ (self):
         return len(self.dataset)
