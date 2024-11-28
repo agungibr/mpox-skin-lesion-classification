7c542aacbc5eb7c4f9409c1a364448f4c241a008
agungibr <agungmalik050@gmail.com>
2024-11-28 21:00:31 +0700

    feat: update

--- utils/getData.py
+++ utils/getData.py
@@ -2,47 +2,37 @@ import cv2 as cv
 import torch
 import numpy as np
 from os import listdir
-from os.path import join
 from torch.utils.data import Dataset
 
 class getImageLabel(Dataset):
     def __init__ (self, 
-                  augmented = './dataset/Augmented Images/Augmented Images/FOLDS_AUG/',
-                  original = './dataset/Original Images/Original Images/FOLDS/',
-                  folds = [1, 2, 3, 4, 5]):
+                  augmented = None,
+                  original = None,
+                  folds = [1, 2, 3, 4, 5],
+                  subdir = ['Train', 'Test', 'Valid']):
         
         self.dataset = []
         to_one_hot = np.eye(6)
 
         for fold in folds:
-            ori_train = (original + f"fold{fold}/Train")
-            for i, pox in enumerate(sorted(listdir(ori_train))):
-                for image_name in listdir(ori_train + "/" + pox):
-                    image = cv.resize(cv.imread(ori_train + "/" + pox + "/" + image_name), (32, 32)) / 255
-                    self.dataset.append([image, to_one_hot[i]])
-            
-            aug = (augmented + f"fold{fold}_AUG/Train")
-            for i, pox in enumerate(sorted(listdir(aug))):
-                for image_name in listdir(aug + "/" + pox):
-                    image = cv.resize(cv.imread(aug + "/" + pox + "/" + image_name), (32, 32)) / 255
-                    self.dataset.append([image, to_one_hot[i]])
+            if original:
+                path = (original + f"fold{fold}/{subdir}")
+                for i, pox in enumerate(sorted(listdir(path))):
+                    for image_name in listdir(path + "/" + pox):
+                        image = cv.resize(cv.imread(path + "/" + pox + "/" + image_name), (32, 32)) / 255
+                        self.dataset.append([image, to_one_hot[i]])
 
-            ori_test = (original + f"fold{fold}/Test")
-            for i, pox in enumerate(sorted(listdir(ori_test))):
-                for image_name in listdir(ori_test + "/" + pox):
-                    image = cv.resize(cv.imread(ori_test + "/" + pox + "/" + image_name), (32, 32)) / 255
-                    self.dataset.append([image, to_one_hot[i]])
-
-            ori_valid = (original + f"fold{fold}/Valid")
-            for i, pox in enumerate(sorted(listdir(ori_valid))):
-                for image_name in listdir(ori_valid + "/" + pox):
-                    image = cv.resize(cv.imread(ori_valid + "/" + pox + "/" + image_name), (32, 32)) / 255
-                    self.dataset.append([image, to_one_hot[i]])
+            if augmented and subdir == 'Train':
+                path = (augmented + f"fold{fold}_AUG/Train")
+                for i, pox in enumerate(sorted(listdir(path))):
+                    for image_name in listdir(path + "/" + pox):
+                        image = cv.resize(cv.imread(path + "/" + pox + "/" + image_name), (32, 32)) / 255
+                        self.dataset.append([image, to_one_hot[i]])
             
     def __getitem__(self, item):
         feature, label = self.dataset[item]
         #return torch.tensor(feature, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
-        label_index = torch.argmax(torch.tensor(label, dtype=torch.float32))  # Convert one-hot to class index
+        label_index = torch.argmax(torch.tensor(label, dtype=torch.float32))
         return torch.tensor(feature, dtype=torch.float32), label_index
     
     def __len__ (self):
