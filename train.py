d0617d1874f2e594f663839320210260a08b075d
agungibr <agungmalik050@gmail.com>
2024-12-02 19:30:56 +0700

    feat: done

--- train.py
+++ train.py
@@ -5,25 +5,21 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from utils.getData import getImageLabel
 from torch.optim import Adam
-from torchvision import models
-from torchvision.models import shufflenet_v2_x0_5
-#from vit_pytorch import ViT
+from models.cnn import SimpleCNN
 
 def main():
     BATCH_SIZE = 32
-    EPOCH = 50
+    EPOCH = 15
     LEARNING_RATE = 0.001
     folds = [1,2,3,4,5]
-    device = 'cpu'
+    DEVICE = 'cpu'
 
-    for fold in folds:
-        train_aug_loader = DataLoader(getImageLabel(augmented=f'./dataset/Augmented Images/Augmented Images/FOLDS_AUG/', folds=[fold], subdir='Train'), batch_size=BATCH_SIZE, shuffle=True)
-        train_ori_loader = DataLoader(getImageLabel(original=f'./dataset/Original Images/Original Images/FOLDS/', folds=[fold], subdir='Train'), batch_size=BATCH_SIZE, shuffle=True)
-        vali_loader = DataLoader(getImageLabel(original=f'./dataset/Original Images/Original Images/FOLDS/', folds=[fold], subdir='Valid'), batch_size=BATCH_SIZE, shuffle=False)
+    train_aug_loader = DataLoader(getImageLabel(augmented=f'./dataset/Augmented Images/Augmented Images/FOLDS_AUG/', folds=folds, subdir=['Train']), batch_size=BATCH_SIZE, shuffle=True)
+    train_ori_loader = DataLoader(getImageLabel(original=f'./dataset/Original Images/Original Images/FOLDS/', folds=folds, subdir=['Train']), batch_size=BATCH_SIZE, shuffle=True)
+    vali_loader = DataLoader(getImageLabel(original=f'./dataset/Original Images/Original Images/FOLDS/', folds=folds, subdir=['Valid']), batch_size=BATCH_SIZE, shuffle=False)
 
-    #model = models.mobilenet_v3_small(pretrained=True)
-    model = shufflenet_v2_x0_5(pretrained=True)
-    model.to(device)
+    model = SimpleCNN(input_dim=32, input_c=3, output=6, device=DEVICE)
+    model.to(DEVICE)
     optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
     loss_function = nn.CrossEntropyLoss()
 
@@ -62,23 +58,22 @@ def main():
             
         loss_train_all.append(train_loss / (len(train_aug_loader) + len(train_ori_loader)))
         loss_vali_all.append(vali_loss / len(vali_loader))
-        print("epoch = ", epoch + 1, "train loss = ", train_loss / (len(train_aug_loader) + len(train_ori_loader)),
-            ", validation loss = ", vali_loss / len(vali_loader))
+        print(f'Epoch {epoch + 1}, Train Loss: {train_loss / (len(train_aug_loader) + len(train_ori_loader))}, Validation Loss: {vali_loss / len(vali_loader)}')
             
-        if (epoch + 1) % 50 == 0:
+        if (epoch + 1) % 15 == 0:
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': train_loss / (len(train_aug_loader) + len(train_ori_loader)),
-            }, "./ShuffleNetV2_" + str(epoch + 1) + ".pt")
+            }, "./SimpleCNN_" + str(epoch + 1) + ".pt")
             
     plt.plot(range(EPOCH), loss_train_all, color="#931a00", label='Training')
     plt.plot(range(EPOCH), loss_vali_all, color="#3399e6", label='Validation')
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend()
-    plt.savefig("./trainingShuffle50.png")
+    plt.savefig("./training.png")
 
 if __name__ == "__main__":
     main()
